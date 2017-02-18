5b646302ec6e408bc320e0799f84d9d731425e46
Julien Martin <martin.julien82@gmail.com>
2017-02-18 13:28:14 +0100

    New configuration system with dist/local

--- server/src/githubApi.js
+++ server/src/githubApi.js
@@ -1,6 +1,6 @@
 const request = require('request');
 const async = require('async');
-const settings = require('../package.json').settings;
+const settings = require('../configuration');
 
 let pulls = [];
 const defaultPull = {
