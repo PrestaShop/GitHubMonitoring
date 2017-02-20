05e4c61ba66e2b5eea0d2f1e013ff44fc3a21273
Julien Martin <martin.julien82@gmail.com>
2017-02-20 15:57:42 +0100

    Add route for websocket connection

--- server/app/routes.js
+++ server/app/routes.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const issuesContainer = require('../lib/issuesContainer');
 
 const routes = [];
 
@@ -8,6 +9,18 @@ routes.push({
   path: '/',
   handler: (request, reply) => {
     reply.file(path.resolve(__dirname, '../../public/index.html'), { confine: false });
+  },
+});
+
+// Route called when a client is connected via websocket.
+routes.push({
+  method: 'GET',
+  path: '/ws',
+  config: {
+    id: 'connection',
+    handler: (request, reply) => {
+      reply(issuesContainer.getIssues());
+    },
   },
 });
 
