b5af0e41f71ee3aaebe66b67a4f481efdda72803
Julien Martin <martin.julien82@gmail.com>
2017-03-07 10:12:27 +0100

    Server initialization moved to an exported method

--- server/app/index.js
+++ server/app/index.js
@@ -1,31 +1,3 @@
-const createServer = require('../lib/server').create;
-const configuration = require('../configuration');
-const fetchData = require('./fetchData');
-const routes = require('./routes');
-const async = require('async');
-const colors = require('colors');
+const initServer = require('./initServer');
 
-async.waterfall([
-  (next) => {
-    process.stdout.write(colors.gray('Fetching issues…\n'));
-    fetchData(next);
-  },
-  (issues, next) => {
-    process.stdout.write(colors.green(issues.length));
-    process.stdout.write(' issue(s) found\n');
-    next();
-  },
-  (next) => {
-    process.stdout.write(colors.gray('Server initialization…\n'));
-    createServer(configuration.server, next);
-  },
-  (server, next) => {
-    server.route(routes);
-    process.stdout.write(colors.green(`Server correctly started and listen for ${configuration.server.port}\n`));
-    next();
-  },
-], (err) => {
-  if (err) {
-    throw err;
-  }
-});
+initServer();
